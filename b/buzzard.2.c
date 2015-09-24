c618471f5ef9e1c922da04ce25e39827ecc3f893
Ondřej Čertík <ondrej.certik@gmail.com>
2015-09-23 19:18:22 -0600

    Add comments

--- b/buzzard.2.c
+++ b/buzzard.2.c
@@ -21,29 +21,55 @@ a(x)
 r(x)
 {
    switch(m[x++]){
-	z 5:	for(w=scanf("%s",s)<1?exit(0),0:L;strcmp(s,&s[m[w+1]]);w=m[w]);
-		w-1 ? r(w+2) : (c 2,c atoi(s))
-	z 12:	I=m[m[1]--]
-	z 15:	f=S[-f]
-	z 1:	c x 
-	z 9:	f *=* S--
-	z 7:	m[f]= *S--;
-		f= *S--
 	z 0:	*++S=f;
 		f=m[I++]
-	z 8:	f= *S --- f
+
+	z 1:	c x
+
 	z 2:	m[++m[1]]=I;
 		I=x
-	z 11:	f=0>f
-	z 4:	*m-=2;c 2
-	z 6:	f=m[f]
-	z 10:	f= *S--/f
+
 	z 3:	a(1);
 		c 2
+
+	z 4:	*m-=2;c 2
+
+    // _read
+	z 5:	for(w=scanf("%s",s)<1?exit(0),0:L;strcmp(s,&s[m[w+1]]);w=m[w]);
+		w-1 ? r(w+2) : (c 2,c atoi(s))
+
+    // @
+	z 6:	f=m[f]
+
+    // !
+	z 7:	m[f]= *S--;
+		f= *S--
+
+    // -
+	z 8:	f= *S --- f
+
+    // *
+	z 9:	f *=* S--
+
+    // /
+	z 10:	f= *S--/f
+
+    // <0
+	z 11:	f=0>f
+
+    // exit
+	z 12:	I=m[m[1]--]
+
+    // echo
 	z 13:	putchar(f);
 		f= *S--
+
+    // key
 	z 14:	*++S=f;
 		f=getchar();
+
+    // _pick
+	z 15:	f=S[-f];
    }
 }
 
